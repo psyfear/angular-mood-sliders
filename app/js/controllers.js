3dc64a4ec2168d99e403c964a7cc1117ac67e131
Jessie Espesor <psyfear@mac.com>
2014-08-09 21:59:02 +0800

    Initial commit

---
+++ app/js/controllers.js
@@ -0,0 +1,103 @@
+'use strict';
+
+/* Controllers */
+
+// var phonecatApp = angular.module('phonecatApp', []);
+
+// phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+//   $http.get('phones/phones.json').success(function(data) {
+//     $scope.phones = data;
+//   });
+
+//   $scope.orderProp = 'age';
+// });
+
+var moodSlidersApp = angular.module('moodSlidersApp', ['ngCookies', 'ngSlider']);
+
+moodSlidersApp.controller('MoodSliderCtrl', ['$scope','$cookies','$http', '$log', function($scope, $cookies, $http, $log) {
+  // $http.get('phones/phones.json').success(function(data) {
+  //   $scope.phones = data;
+  // });
+
+  if($cookies.hugSlider) {
+    $scope.hugSlider = $cookies.hugSlider;
+  }
+  if($cookies.hugSlider) {
+    $scope.hungerSlider = $cookies.hungerSlider;
+  }
+  if($cookies.hugSlider) {
+    $scope.funSlider = $cookies.funSlider;
+  }
+  if($cookies.hugSlider) {
+    $scope.socialSlider = $cookies.socialSlider;
+  }
+  if($cookies.hugSlider) {
+    $scope.energySlider = $cookies.energySlider;
+  }
+  if($cookies.hugSlider) {
+    $scope.comfortSlider = $cookies.comfortSlider;
+  }
+
+  $scope.hugOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+  $scope.hungerOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+  $scope.funOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+  $scope.socialOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+  $scope.energyOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+  $scope.comfortOptions = {
+    from: 1,
+    to: 100,
+    step: 1,
+    scale: [0, '|', 50, '|' , 100],
+    dimension: " %"
+  };
+
+  $scope.hugChange = function() {
+    $cookies.hugSlider = $scope.hugSlider
+  };
+  $scope.hungerChange = function() {
+    $cookies.hungerSlider = $scope.hungerSlider
+  };
+  $scope.funChange = function() {
+    $cookies.funSlider = $scope.funSlider
+  };
+  $scope.socialChange = function() {
+    $cookies.socialSlider = $scope.socialSlider
+  };
+  $scope.energyChange = function() {
+    $cookies.energySlider = $scope.energySlider
+  };
+  $scope.comfortChange = function() {
+    $cookies.comfortSlider = $scope.comfortSlider
+  };
+
+}]);
---
+++ server.js
@@ -0,0 +1,6 @@
+var redis = require("redis");
+var client = redis.createClient();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
